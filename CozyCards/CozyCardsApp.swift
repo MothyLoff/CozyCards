d2bc1116b172f48258cffa7f2f6742f7e770755d
timothyloff <tiqaiteam@gmail.com>
2026-07-09 16:05:17 +0300

    feat(library): LibraryStore with environment injection

--- CozyCards/CozyCardsApp.swift
+++ CozyCards/CozyCardsApp.swift
@@ -4,13 +4,17 @@ import SwiftUI
 
 @main
 struct CozyCardsApp: App {
-    
-    
+
+
+    @State private var libraryStore = LibraryStore(repository: InMemoryLibraryRepository())
+
+
     var body: some Scene {
         WindowGroup {
             RootView()
+                .environment(libraryStore)
         }
     }
-    
-    
+
+
 }
