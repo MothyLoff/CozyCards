68a98534585bdb457d28e0127a686e30d110940c
“Shakirova <106487950+Defekret@users.noreply.github.com>
2026-07-09 15:51:30 +0300

    new file Models and change CozyCardsApp file

--- CozyCards/CozyCardsApp.swift
+++ CozyCards/CozyCardsApp.swift
@@ -1,16 +1,13 @@
 import SwiftUI
-
+import SwiftData
 
 
 @main
 struct CozyCardsApp: App {
-    
-    
     var body: some Scene {
         WindowGroup {
             RootView()
         }
+        .modelContainer(for: [Chat.self, WordDictionary.self, WordCard.self])
     }
-    
-    
 }
